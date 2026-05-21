ea44821b109cedfb2d16399af8fb866cfbddcfcc
Joselyn Guitz <joselynguitz@gmail.com>
2026-05-21 12:56:35 -0600

    Se modifico el codigo de .js y html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 let data = [
-    { id: 1, texto: 'Holaaa', estadoConpletado: false },
+    { id: 1, texto: 'Holaaa', estadoConpletado: true },
     { id: 2, texto: 'Joselyn', estadoConpletado: false }
 ]
 
@@ -8,79 +8,8 @@ let input = document.querySelector('#inputTarea')
 let boton = document.querySelector('#btnTarea')
 let tareas = document.querySelector('#tareas')
 
-if (data.length > 0) {
-    for (let i = 0; i <= data.length - 1; i++) {
-        //Dibujar div de tarea
-        let div = document.createElement('div')
-        div.className = 'd-flex w-50 justify-content-between align-items-baseline border-bottom border-light-subtle p-2'
-
-        //Dibujar checkbox
-        let checkbox = document.createElement('input')
-        checkbox.setAttribute('type', 'checkbox')
-        checkbox.setAttribute('id', data[i].id)
-        if (data[i].estadoConpletado) {
-            checkbox.checked = true
-        }
-        checkbox.className = 'me-2 fs-3'
-
-        //Dibujando el label
-        let label = document.createElement('label')
-        label.className = 'fs-3'
-        //Pendienye algo ... (insertar texto en el label)
-
-        if (data[i].estadoConpletado) {
-            checkbox.checked = true;
-            label.classList.add('text-decoration-line-through')
-        } else {
-            label.classList.remove('text-decoration-line-through')
-
-
-        }
-
-        checkbox.addEventListener('click', (event) => {
-            // console.log(event.target.id)
-            let tareaAbuscar = data.find(item => item.id == event.target.id)
-            tareaAbuscar.estadoConpletado = !tareaAbuscar.estadoConpletado
-            //.toggle colocamos no colocamos
-
-            if (tareaAbuscar.estadoConpletado) {
-                checkbox.checked = true;
-                label.classList.add('text-decoration-line-through')
-            } else {
-                label.classList.remove('text-decoration-line-through')
-
-
-            }
-
-        })
-
-        //Dibujar el sup
-        let sup = document.createElement('sup')
-        sup.insertAdjacentHTML('afterbegin', '<i class="bi bi-trash-fill"></i>')
-        // sup.textContent = 'X'
-        sup.className = 'text-danger fs-3 '
-
-        sup.addEventListener('click', () => {
-            div.remove()
-        })
-
-
-        //Se coloco aqui osea colocar el texto para que tenga un valor
-        label.textContent = data[i].texto
-        div.append(checkbox)
-        div.append(label)
-        div.append(sup)
-        // console.log(input.value)
-        tareas.append(div)
-
-    }
-
-}
-
-
-//Agregar elementos al div
-boton.addEventListener('click', () => {
-
+    
+const DibujarElementos = (info = null, i = null) => {
     //Dibujar div de tarea
     let div = document.createElement('div')
     div.className = 'd-flex w-50 justify-content-between align-items-baseline border-bottom border-light-subtle p-2'
@@ -88,21 +17,12 @@ boton.addEventListener('click', () => {
     //Dibujar checkbox
     let checkbox = document.createElement('input')
     checkbox.setAttribute('type', 'checkbox')
-    checkbox.className = 'me-2 fs-3'
-
-
+    checkbox.className = 'checkbox me-2 fs-3'
 
     //Dibujando el label
     let label = document.createElement('label')
     label.className = 'fs-3'
     //Pendienye algo ... (insertar texto en el label)
-
-    checkbox.addEventListener('click', (event) => {
-        //.toggle colocamos no colocamos
-        label.classList.toggle('text-decoration-line-through')
-
-
-    })
 
     //Dibujar el sup
     let sup = document.createElement('sup')
@@ -110,21 +30,63 @@ boton.addEventListener('click', () => {
     // sup.textContent = 'X'
     sup.className = 'text-danger fs-3 '
 
-    sup.addEventListener('click', () => {
-        div.remove()
-
-    })
-
-
-    //Se coloco aqui osea colocar el texto para que tenga un valor
-    label.textContent = input.value
+    if (info == null || i == null) {
+        checkbox.setAttribute('id', data.length + 1)
+        label.textContent = input.value
+    } else {
+        checkbox.setAttribute('id', info[i].id)
+        label.textContent = info[i].texto
+    }
     div.append(checkbox)
     div.append(label)
     div.append(sup)
-    data.push({ id: data.length + 1, texto: input.value, estadoConpletado: false })
-    // console.log(input.value)
-    tareas.append(div)
+
+    return { div, checkbox, label, sup }
+}
+
+const DibujarTodo = () => {
+    if (data.length > 0) {
+        for (let i = 0; i <= data.length - 1; i++) {
+
+            const { div, checkbox, label, sup } = DibujarElementos(data, i);
+
+            if (data[i].estadoConpletado) {
+                checkbox.checked = true;
+                label.classList.add('text-decoration-line-through')
+            } else {
+                checkbox.checked = false
+                label.classList.remove('text-decoration-line-through')
+            }
+            tareas.append(div)
+        }
+    }
+}
+
+//Agregar elementos al div
+boton.addEventListener('click', () => {
+
+    data.push(
+        { id: data.length + 1, texto: input.value, estadoConpletado: false }
+    )
+    tareas.innerHTML = ''
+    DibujarTodo();
     input.value = ''
 
-
 })
+
+tareas.addEventListener('click', (event) => {
+    if (event.target.classList.contains('checkbox')) {
+        let tareaAbuscar = data.find(item => item.id == event.target.id)
+        tareaAbuscar.estadoConpletado = !tareaAbuscar.estadoConpletado
+        //.toggle colocamos no colocamos
+
+        if (tareaAbuscar.estadoConpletado) {
+            event.target.nextSibling.classList.add('text-decoration-line-through')
+        } else {
+            event.target.nextSibling.classList.remove('text-decoration-line-through')
+        }
+    }
+})
+
+DibujarTodo()
+// target contains nextsibliling
